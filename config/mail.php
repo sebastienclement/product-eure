5a6d2eae12e39d454a062d8b952bbde20d7b54f2
bastien <delavienne.b@gmail.com>
2018-02-27 11:27:52 +0100

    mail producer

--- config/mail.php
+++ config/mail.php
@@ -130,7 +130,7 @@ return [
 
     //a enlever en prod , Ok pour dev
     'to' => [
-      'address' => 's.clement.n@gmail.com',
+      'address' => 'mabasewf3@gmail.com',
       'name' => 'Johnny'
     ]
 
